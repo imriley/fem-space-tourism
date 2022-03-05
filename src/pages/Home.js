5b6b84f6c8278150a58aa80d8ac6d0ab5a2783b0
riley <vishhh0320@gmail.com>
2022-03-06 00:28:48 +0530

    added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,21 @@
 import React from "react";
-import "../styles/pages/home.scss"
+import "../styles/pages/home.scss";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="home">
+      <div className="home__content">
+        <div className="home__subheading">So, you want to travel to</div>
+        <h1 className="home__heading">Space</h1>
+        <div className="home__body">
+          Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out
+          of this world experience!
+        </div>
+      </div>
+      <div className="home__btn">
+        <Link to="/">explore</Link>
+      </div>
+    </div>
+  );
 }
