0985fe1caffbae5cc8e350eb7f8c09fb630b0429
riley <vishhh0320@gmail.com>
2022-03-09 21:19:49 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Destination from "./pages/Destination";
 import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -11,12 +12,13 @@ function App() {
   return (
     <Router>
       <div className={`page ${page}`}>
-        <div className={`container ${page}`}>
+        <div className="container">
           <Navbar />
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/destination" element={<Destination />} />
             <Route exact path="/crew" element={<Crew />} />
+            <Route exact path="/technology" element={<Technology />} />
           </Routes>
         </div>
       </div>
---
+++ src/pages/Technology.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import data from "../assets/data.json";
+
+export default function Technology() {
+  let tech = data.technology;
+  const [title, setTitle] = useState(tech.lv.name);
+  const [description, setDescription] = useState(tech.lv.description);
+
+  const changeData = (newTech) => {
+    setTitle(newTech.name);
+    setDescription(newTech.description);
+  };
+  return (
+    <div className="technology">
+      <div className="technology__heading">
+        <span>03</span>Space Launch 101
+      </div>
+      <div className="technology__wrapper">
+        <div className="technology__image" data-image={title}></div>
+        <div className="technology__content">
+          <div className="technology__btns">
+            <div className={`btn${title.toLowerCase() === "launch vehicle" ? " active" : ""}`} onClick={() => changeData(tech.lv)}>
+              <span>1</span>
+            </div>
+            <div className={`btn${title.toLowerCase() === "spaceport" ? " active" : ""}`} onClick={() => changeData(tech.sp)}>
+              <span>2</span>
+            </div>
+            <div className={`btn${title.toLowerCase() === "space capsule" ? " active" : ""}`} onClick={() => changeData(tech.sc)}>
+              <span>3</span>
+            </div>
+          </div>
+          <div className="technology__details">
+            <div className="technology__subheading">The terminology...</div>
+            <div className="technology__title">{title}</div>
+            <div className="technology__description">{description}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
